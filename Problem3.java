154a26d33788a22a424dfe56b1dedfa0e08ac92a
SidhantPuntambekar <Admin@falcon.local>
2018-10-29 14:15:17 -0600

    Problem 3 initial commit. Code errors when I implement isPrime method.

---
+++ Problem3.java
@@ -0,0 +1,30 @@
+public class Problem3
+{
+    private static long x = 600851475143L;
+
+    public static long PrimeNums()
+    {
+        for (long z = (int) Math.sqrt(x); z >= 1; z--)
+        {
+            if (x%z == 0)
+                return z;
+        }
+        return x;
+    }
+
+    public static boolean isPrime(long z)
+    {
+        for (long i=2; i<=z; i++) 
+        {
+            if (z%i == 0) 
+              return false;
+        }
+          return true;
+        
+    } 
+
+    public static void main(String [] args)
+    {
+        System.out.println(PrimeNums());
+    }
+}

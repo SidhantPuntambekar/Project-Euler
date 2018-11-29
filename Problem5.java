21a9b014313f4cf50eb4b6b8af5be5be995ad90c
SidhantPuntambekar <Admin@falcon.local>
2018-11-29 14:48:44 -0700

    Finished Problem 5

--- Problem5.java
+++ Problem5.java
@@ -4,17 +4,19 @@ public class Problem5
     public static void main(String[] args)
     {
         
-        long number = 2520;
+        
         long smallest = 0;
         for (int k = 2520; k < 1000000000; k+=2520)
         {
             boolean isMultiple = true;
             for (int i = 1; i <= 20; i++)
+            {
+                if (k % i != 0)
                 {
-                    if (number % i != 0)
-                        isMultiple = false;
-                        break;
+                    isMultiple = false;
+                    break;
                 }
+            }
             if (isMultiple)
             {
                 smallest = k;

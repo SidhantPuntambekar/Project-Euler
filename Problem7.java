a20dcaa33c035b4a007933b8648ebc3aec57e0af
SidhantPuntambekar <Admin@falcon.local>
2018-11-29 15:07:58 -0700

    Started Problem 7

---
+++ Problem7.java
@@ -0,0 +1,29 @@
+public class Problem7
+{
+    public static boolean isPrime()
+    {
+        if (z == 2)
+        {
+            return true;
+        }
+        for (long i=3; i<z; i+=2) 
+        { 
+            if (z%i == 0) 
+              return false;
+        }
+          return true;
+    }
+
+    public static int LongPrime()
+    {
+        for (int i = 0; i <=100000000; i++)
+        {
+            int count = 0;
+            
+        }
+    }
+    public static void main (String[] args)
+    {
+
+    }
+}

d439e2a93a65f848d0d8a1ea4b5bd27cd8e7381f
SidhantPuntambekar <Admin@falcon.local>
2018-11-29 14:39:57 -0700

    Changed while to for loops and added more logic

--- Problem5.java
+++ Problem5.java
@@ -1,18 +1,26 @@
 public class Problem5
 {
-    public static int SmallestMultiple(int num)
-    {
-        int number = num;
-        for (int i = 1; i <= 20; i++)
-        {
-            if (number % i == 0)
-                number++;
-        }
-        return number;
-    }
 
     public static void main(String[] args)
     {
-
+        
+        long number = 2520;
+        long smallest = 0;
+        for (int k = 2520; k < 1000000000; k+=2520)
+        {
+            boolean isMultiple = true;
+            for (int i = 1; i <= 20; i++)
+                {
+                    if (number % i != 0)
+                        isMultiple = false;
+                        break;
+                }
+            if (isMultiple)
+            {
+                smallest = k;
+                break;
+            }
+        }
+        System.out.println(smallest);
     }
 }

49b5c0d142a3d4cfa3a07233c7203d5765f94a22
SidhantPuntambekar <Admin@falcon.local>
2018-12-03 16:41:34 -0700

    Started Problem 8. Number converted to .txt file

---
+++ Problem8.java
@@ -0,0 +1,23 @@
+import java.io.File; 
+import java.util.Scanner; 
+public class Problem8
+{
+    public static void main(String[] args) throws Exception 
+    { 
+        int[] digits = new int[1000];
+
+        // pass the path to the file as a parameter 
+        File file = new File("Problem8Input.txt"); 
+        Scanner sc = new Scanner(file);
+        int digitsInitialized = 0; 
+        while (sc.hasNextLine()) 
+        {
+            String fileLine = sc.nextLine();
+            for (int i = 0; i < fileLine.length(); i++) {
+                digits[digitsInitialized] = fileLine.charAt(i) - '0';
+                digitsInitialized++;
+            }
+        } 
+        
+    } 
+}

737ae05c8efcaf17f361efc33e2d83a666bcb286
Sidhant Puntambekar <sidhantnp@yahoo.com>
2018-12-09 09:53:53 -0700

    Initial commit Problem 12

---
+++ Problem12.java
@@ -0,0 +1,33 @@
+public class Problem12
+{
+    public static int Divisors(int n)
+    {
+        int count = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (n%i == 0)
+                count++;
+        }
+        return count;
+    }
+
+    public static void main (String[] args)
+    {
+        int a = 0;
+        int x = 2;
+        boolean bool = false;
+        while (!bool)
+        {
+            if (Divisors(a) > 500)
+            {
+                System.out.println(a);
+                bool = true;
+            }
+            else
+            {
+                a += x;
+                x++;
+            }
+        }
+    }
+}

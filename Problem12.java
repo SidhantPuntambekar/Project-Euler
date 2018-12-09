08b715c5c7bae47b38ea145c885fe14843f7bd5d
Sidhant Puntambekar <sidhantnp@yahoo.com>
2018-12-09 12:46:51 -0700

    Sped up execution time

--- Problem12.java
+++ Problem12.java
@@ -3,7 +3,7 @@ public class Problem12
     public static int Divisors(int n)
     {
         int count = 0;
-        for (int i = 1; i <= n; i++)
+        for (int i = 1; i*i <= n; i++)
         {
             if (n%i == 0)
                 count++;

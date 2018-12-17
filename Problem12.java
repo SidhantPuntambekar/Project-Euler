46465280517ccfbd4090c950ba45af150fd9bf42
Sidhant Puntambekar <sidhantnp@yahoo.com>
2018-12-17 12:01:02 -0700

    Finished Problem 12

--- Problem12.java
+++ Problem12.java
@@ -6,10 +6,17 @@ public class Problem12
         for (int i = 1; i*i <= n; i++)
         {
             if (n%i == 0)
-                count++;
+            {
+                count += 2;
+                if (i*i == n)
+                {
+                    count--;
+                }
+            }   
         }
         return count;
     }
+
 
     public static void main (String[] args)
     {

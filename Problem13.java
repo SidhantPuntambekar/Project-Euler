c94b068711270a0910493461df7e76d6da2411e8
Sidhant Puntambekar <sidhantnp@yahoo.com>
2018-12-17 12:15:48 -0700

    Initial commit. Problem 13

---
+++ Problem13.java
@@ -0,0 +1,11 @@
+import java.io.File; 
+import java.util.Scanner; 
+import java.math.BigInteger;
+public class Problem13
+{
+    public static void main(String[] args)
+    {
+        File file = new File("Problem8Input.txt"); 
+        Scanner sc = new Scanner(file);
+    }
+}

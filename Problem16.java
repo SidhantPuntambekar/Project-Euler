a8dbb1c66eacce6182da461bf60219ff432ba747
Sidhant Puntambekar <sidhantnp@yahoo.com>
2018-12-24 09:17:12 -0700

    Initial Commit Problem 16

---
+++ Problem16.java
@@ -0,0 +1,19 @@
+public class Problem16
+{
+    public static double PowerSum(double a, double b)
+    {
+        double Power = 0;
+        Power = Math.pow(a,b);
+        return Power;
+        int sum = 0;
+        for (int i = 0; i < Power.length; i++)
+        {
+            
+        } 
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(PowerSum(2,15));
+    }
+}

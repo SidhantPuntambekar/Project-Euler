0969b845e7d04e54faa4c863498bff02eef0cecb
Sidhant Puntambekar <sidhantnp@yahoo.com>
2018-12-25 13:44:27 -0700

    Finished Problem 15

--- Problem15.java
+++ Problem15.java
@@ -2,6 +2,21 @@ public class Problem15
 {
     public static long binomCoefficient(int a, int b)
     {
-        
+        if (b > (a-b))
+        {
+            b = a - b;
+        }
+        long c = 1;
+		for (int i = 0; i < b; i++)
+		{
+			c = c * (a - i);
+			c = c / (i + 1);			
+		}
+		return c;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(binomCoefficient(40, 20));
     }
 }

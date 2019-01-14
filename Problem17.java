d731989672d4e4e29b3214254f112a48d4fd994e
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-01-14 15:59:14 -0700

    Print out String version to find issue in code

--- Problem17.java
+++ Problem17.java
@@ -3,62 +3,65 @@ public class Problem17
     public static long countCharacters(int n)
     {
         long sum = 0;
-        for (int i = 0; i <= n; i++)
+        for (int i = 1; i <= n; i++)
         {
-            sum += returnCharacters(i);
+            sum += returnCharacters(i).length();
         }
         return sum;
     }
 
-    public static int returnCharacters(int n)
+    public static String returnCharacters(int n)
     {
-        int letters = 0;
+        String letters = "";
         switch (n) 
         {
-            case 0: letters = 0; break;
-            case 1: letters = "One".length(); break;
-            case 2: letters = "Two".length(); break;
-            case 3: letters = "Three".length(); break;
-            case 4: letters = "Four".length(); break;
-            case 5: letters = "Five".length(); break;
-            case 6: letters = "Six".length(); break;
-            case 7: letters = "Seven".length(); break;
-            case 8: letters = "Eight".length(); break;
-            case 9: letters = "Nine".length(); break;
-            case 10: letters = "Ten".length(); break;
-            case 11: letters = "Eleven".length(); break;
-            case 12: letters = "Twelve".length(); break;
-            case 13: letters = "Thirteen".length(); break;
-            case 14: letters = "Fourteen".length(); break;
-            case 15: letters = "Fifteen".length(); break;
-            case 16: letters = "Sixteen".length(); break;
-            case 17: letters = "Seventeen".length(); break;
-            case 18: letters = "Eighteen".length(); break;
-            case 19: letters = "Nineteen".length(); break;
-            case 20: letters = "Twenty".length(); break;
-            case 30: letters = "Thirty".length(); break;
-            case 40: letters = "Forty".length(); break;
-            case 50: letters = "Fifty".length(); break;
-            case 60: letters = "Sixty".length(); break;
-            case 70: letters = "Seventy".length(); break;
-            case 80: letters = "Eighty".length(); break;
-            case 90: letters = "Ninety".length(); break;
-            case 100: letters = "OneHundred".length(); break;
-            case 200: letters = "TwoHundred".length(); break;
-            case 300: letters = "ThreeHundred".length(); break;
-            case 400: letters = "FourHundred".length(); break;
-            case 500: letters = "FiveHundred".length(); break;
-            case 600: letters = "SixHundred".length(); break;
-            case 700: letters = "SevenHundred".length(); break;
-            case 800: letters = "EightHundred".length(); break;
-            case 900: letters = "NineHundred".length(); break;
-            case 1000: letters = "OneThousand".length(); break;
+            case 0: letters = ""; break;
+            case 1: letters = "One"; break;
+            case 2: letters = "Two"; break;
+            case 3: letters = "Three"; break;
+            case 4: letters = "Four"; break;
+            case 5: letters = "Five"; break;
+            case 6: letters = "Six"; break;
+            case 7: letters = "Seven"; break;
+            case 8: letters = "Eight"; break;
+            case 9: letters = "Nine"; break;
+            case 10: letters = "Ten"; break;
+            case 11: letters = "Eleven"; break;
+            case 12: letters = "Twelve"; break;
+            case 13: letters = "Thirteen"; break;
+            case 14: letters = "Fourteen"; break;
+            case 15: letters = "Fifteen"; break;
+            case 16: letters = "Sixteen"; break;
+            case 17: letters = "Seventeen"; break;
+            case 18: letters = "Eighteen"; break;
+            case 19: letters = "Nineteen"; break;
+            case 20: letters = "Twenty"; break;
+            case 30: letters = "Thirty"; break;
+            case 40: letters = "Forty"; break;
+            case 50: letters = "Fifty"; break;
+            case 60: letters = "Sixty"; break;
+            case 70: letters = "Seventy"; break;
+            case 80: letters = "Eighty"; break;
+            case 90: letters = "Ninety"; break;
+            case 100: letters = "OneHundred"; break;
+            case 200: letters = "TwoHundred"; break;
+            case 300: letters = "ThreeHundred"; break;
+            case 400: letters = "FourHundred"; break;
+            case 500: letters = "FiveHundred"; break;
+            case 600: letters = "SixHundred"; break;
+            case 700: letters = "SevenHundred"; break;
+            case 800: letters = "EightHundred"; break;
+            case 900: letters = "NineHundred"; break;
+            case 1000: letters = "OneThousand"; break;
         }
-        if (letters == 0){
-            int length = (int)(Math.log10(n)+1); //how many digits does the number have? 
+        if (letters.length() == 0)
+        {
+            int length = (int)(Math.log10(n)+1);  
             int splitNumber;
-            for (int i=1;i<=length;i++){
-                if (i == 3) letters += 3; //add "and" to the total letters if necessary
+            for (int i=1;i<=length;i++)
+            {
+                if (i == 3) 
+                    letters += "and"; 
                 splitNumber = (int) (n % Math.pow(10, i)); 
                 letters += returnCharacters(splitNumber);
                 n -= splitNumber;
@@ -69,6 +72,9 @@ public class Problem17
 
     public static void main(String[] args)
     {
-        System.out.println(countCharacters(1000));
+        //System.out.println(countCharacters(1000));
+        for (int i = 1; i <= 1000; i++)
+            System.out.println(returnCharacters(i));
+        // //System.out.println(returnCharacters(40));
     }
 }

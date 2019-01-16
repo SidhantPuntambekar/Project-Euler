6273c6bd7fa93afc2cb7d384c47a455f66ece6c4
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-01-16 14:45:14 -0700

    Finished Problem 21

--- Problem21.java
+++ Problem21.java
@@ -2,23 +2,27 @@ public class Problem21
 {
     public static int Divisors(int n)
     {
-        int count = 0;
-        for (int i = 1; i*i <= n; i++)
-        {
-            if (n%i == 0)
-            {
-                count += 2;
-                if (i*i == n)
-                {
-                    count--;
-                }
-            }   
-        }
-        return count;
+        int sum = 0;
+		for (int i = 1; i < n; i++) {
+			if (n % i == 0)
+				sum += i;
+		}
+		return sum;
     }
-    
+
+    public static boolean isAmicable(int n)
+    {
+        int amicable = Divisors(n);
+		return amicable != n && Divisors(amicable) == n;
+    }
+
     public static void main(String[] args)
     {
-
+        int sum = 0;
+		for (int i = 1; i < 10000; i++) {
+			if (isAmicable(i))
+				sum += i;
+		}
+		System.out.println(sum);
     }
 }

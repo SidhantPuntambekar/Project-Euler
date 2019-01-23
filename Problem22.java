4fb57cc0cc424f549c0cc66a48f553337614e1aa
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-01-23 12:31:09 -0700

    Finished values method

--- Problem22.java
+++ Problem22.java
@@ -16,7 +16,18 @@ public class Problem22
     public static int values()
     {
         char[] Letters = {'1', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-        
+        char[] tempName = s.toCharArray();
+
+        int sum = 0;
+        for (char c : tempName) 
+        {
+            for (int j = 0; j < ABC.length; j++) 
+            {
+                if (c == ABC[j])
+                    sum += j;
+            }
+        }
+        return sum;
     }
 
     public static void main(String[] args)

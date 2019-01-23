8648693fff6c031161b4a4da4df31a0a4217f3ce
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-01-23 13:17:35 -0700

    Finished Problem 23

--- Problem23.java
+++ Problem23.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+
 public class Problem23
 {
+    private static final int MAX = 28123;
     public static boolean isAbundant(int n) {
         int sum = 1;
         int end = (int) Math.sqrt(n);
@@ -23,6 +26,33 @@ public class Problem23
 
     public static void main(String[] args)
     {
+        int sum = 0;
+        int temp;
+        ArrayList<Integer> abundantNumbers = new ArrayList<Integer>();
+        ArrayList<Integer> abundantSums = new ArrayList<Integer>();
 
+        for (int i = 12; i <= MAX; i++) {
+            if(isAbundant(i)) {
+                abundantNumbers.add(i);
+            }
+        }
+
+        int listSize = abundantNumbers.size();
+
+        for(int i = 0; i < listSize; i++) {
+             for (int j = i; j < listSize; j++) {
+
+                 temp = abundantNumbers.get(i) + abundantNumbers.get(j);
+                 abundantSums.add(temp);
+             }          
+        }
+
+        for (int i = 1; i <= MAX; i++) { 
+                if (abundantSums.contains(i) == false) {
+                    sum += i;
+                }
+        }
+
+        System.out.println(sum);
     }
 }

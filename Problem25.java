41edb3e1ca66ba3be16291721cab78bfd92fdb1e
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-01-24 15:43:13 -0700

    Errors with sum

--- Problem25.java
+++ Problem25.java
@@ -7,12 +7,12 @@ public class Problem25
         BigInteger term2 = BigInteger.valueOf(1);   
         int index = 2;
 
-        while (term2.toString().length < 1000)
+        while (term2.toString().length() < 1000)
         {
-            BigInteger sum = term1 + term2;
+            BigInteger sum = term1.add(term2);
             term1 = term2;
             term2 = sum;
         }
-        return sum;
+        System.out.println(sum);
     }
 }

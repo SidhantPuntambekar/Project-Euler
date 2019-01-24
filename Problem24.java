20a95518533769e6f7e9877273b1afb073d1a60b
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-01-24 10:36:34 -0700

    Finished Problem 24

--- Problem24.java
+++ Problem24.java
@@ -8,7 +8,7 @@ public class Problem24
         
         for (int i = 0; i < 999999; i++) 
         {
-            if (!Library.nextPermutation(array))
+            if (!nextPermutation(array))
                 throw new AssertionError();
         }
 
@@ -16,5 +16,35 @@ public class Problem24
 		for (int i = 0; i < array.length; i++)
 			answer += array[i];
 		System.out.println(answer);
+    }
+
+    public static boolean nextPermutation(int[] array) 
+    {
+            int i = array.length - 1;
+            while (i > 0 && array[i - 1] >= array[i])
+                i--;
+
+            if (i <= 0)
+                return false;
+
+            int j = array.length - 1;
+            while (array[j] <= array[i - 1])
+                j--;
+            
+            int temp = array[i - 1];
+            array[i - 1] = array[j];
+            array[j] = temp;
+            
+            
+            j = array.length - 1;
+            while (i < j) {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+
+            return true;
     }
 }

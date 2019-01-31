ca46e13dd04e29b599a278c2c31a3249132e7ba3
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-01-30 21:04:19 -0700

    Finished Problem 29

--- Problem29.java
+++ Problem29.java
@@ -5,12 +5,18 @@ public class Problem29
     public static void main(String[] args)
     {
         ArrayList<BigInteger> set = new ArrayList<BigInteger>();
+        BigInteger value;
         for (int a = 2; a <= 100; a++) 
         {
-			for (int b = 2; b <= 100; b++)
-                set.add(BigInteger.valueOf(a).pow(b));
+            for (int b = 2; b <= 100; b++)
+            {
+                value = BigInteger.valueOf(a).pow(b);
+                if (!set.contains(value))
+                {
+                    set.add(value);
+                }
+            }
         }
         System.out.println(set.size());
-
     }
 }

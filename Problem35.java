f5e71cfff35670b818b558f67a35a69c6640623c
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-02-04 16:38:28 -0700

    Started Problem 35

---
+++ Problem35.java
@@ -0,0 +1,43 @@
+public class Problem35
+{
+    public static void main(String[] args) 
+    {
+        int count = 0; 
+        for (long i = 0; i < isPrime.length; i++)
+        {
+            if (isCircularPrime(i))
+            {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static boolean isCircularPrime(long y)
+    {   
+        String possibleCircularPrime = Long.toString(y);
+        for (int i = 0; i < possibleCircularPrime.length(); i++) 
+        {
+			if (!isPrime[Long.parseLong(possibleCircularPrime.substring(i) + possibleCircularPrime.substring(0, i))])
+				return false;
+        }
+        return true;
+    }
+
+    public static boolean isPrime(long z)
+    {
+        if (z == 2)
+        {
+            return true;
+        }
+        for (int i = 2; i < z; i++)
+        {
+            if (z%i == 0)
+            {
+                return false; 
+            }
+        }
+        return true;
+    }
+
+}

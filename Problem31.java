882def7768d67b94d5a524423628156fb7c3b875
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-02-06 14:48:47 -0700

    Finished Problem 31

--- Problem31.java
+++ Problem31.java
@@ -2,6 +2,17 @@ public class Problem31
 {
     public static void main(String[] args) 
     {
-        
+        int sum = 200;
+	    int[] coinValues = { 1, 2, 5, 10, 20, 50, 100, 200 };
+	    int[] combinations = new int[sum + 1];
+        combinations[0] = 1;
+        for (int i = 0; i < coinValues.length; i++) 
+        {
+            for (int j = coinValues[i]; j <= sum; j++) 
+            {
+				combinations[j] += combinations[j - coinValues[i]];
+			}
+        }
+        System.out.println(combinations[sum]);
     }
 }

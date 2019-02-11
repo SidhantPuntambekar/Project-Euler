641b4ea25ed682b43b8731fb68912f65bd5a5468
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-02-11 16:16:45 -0700

    Constructed main method and pandigital method

---
+++ Problem38.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+public class Problem38
+{
+    public static void main(String[] args) 
+    {
+        int max = -1; 
+        for (int i = 2; i < 9; i++)
+        {
+            for (int j = 1; j < Math.pow(10,9 / i); i++)
+            {
+                String concatenated = "";
+                for (int k = 1; k <= i; k++)
+                {
+                    concatenated += i * j;
+                }
+                if (isPandigital(concatenated))
+                {
+                    max = Math.max(Integer.parseInt(concatenated), max);
+                }
+            }
+        }
+        System.out.println(max);
+    }
+
+    public static boolean isPandigital(String x) 
+    {
+		if (x.length() != 9)
+			return false;
+		char[] temp = x.toCharArray();
+		Arrays.sort(temp);
+		return new String(temp).equals("123456789");
+	}
+}

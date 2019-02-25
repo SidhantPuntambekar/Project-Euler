592e8439795ef6cae1658bdc4a59644b061e2a06
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-02-25 16:07:21 -0700

    Started Problem 41

---
+++ Problem41.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+public class Problem41
+{
+    public static void main(String[] args) 
+    {
+        int primeNumber = 0;
+        for (int i = 0; i < 1000000; i++)
+        {
+            if (isPrime(primeNumber) && isPandigital(primeNumber + ""))
+            {
+                primeNumber += i;
+            }
+        }
+        System.out.println(primeNumber);
+    }
+
+    public static boolean isPrime(long z)
+    {
+        if (z == 2)
+        {
+            return true;
+        }
+        for (long i=3; i<z; i+=2) 
+        { 
+            if (z%i == 0) 
+              return false;
+        }
+          return true;
+    } 
+
+    public static boolean isPandigital(String x) 
+    {
+		if (x.length() != 9)
+			return false;
+		char[] temp = x.toCharArray();
+		Arrays.sort(temp);
+		return new String(temp).equals("123456789");
+	}
+
+}

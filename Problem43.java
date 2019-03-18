f378c83268c29d94f67696c4cae26f3333dfc078
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-03-18 16:51:56 -0600

    Finished Problem 43

--- Problem43.java
+++ Problem43.java
@@ -3,7 +3,7 @@ public class Problem43
 {
     public static void main(String[] args) 
     {
-        int[] nums = {1,2,3,4,5,6,7,8,9};
+        int[] nums = {0,1,2,3,4,5,6,7,8,9};
         long sum = 0; 
         while (!Arrays.equals(nums, new int[]{9,8,7,6,5,4,3,2,1,0}))
         {

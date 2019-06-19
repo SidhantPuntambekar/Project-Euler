05dfd1191d89972e5251c84affe3ca64cabf60b6
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-06-19 09:32:21 -0600

    Added logic for checking if t,p,h equal each other

--- Problem45.java
+++ Problem45.java
@@ -10,6 +10,22 @@ public class Problem45
             long t = (long) (triangle)*(triangle + 1)/2;
             long p = (long) (pentagonal) * (3*pentagonal + 1)/2; 
             long h = (long) (hexagonal) * (2*hexagonal-1);
+            if (t == p && t == h)
+            {
+                break;
+            }
+            else if(t <= p && t <= h) 
+            {
+				triangle++;
+            } 
+            else if(p <= t && p <= h) 
+            {
+				pentagonal++;
+            } 
+            else if(h <= t && h <= p) 
+            {
+				hexagonal++;
+			}
         }
     }
     

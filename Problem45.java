400e8c302ca8a189a58772f3aa475499a069cc9f
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-06-19 09:27:56 -0600

    Computes next number for each sequence

--- Problem45.java
+++ Problem45.java
@@ -5,7 +5,12 @@ public class Problem45
         int triangle = 286;
         int pentagonal = 166;
         int hexagonal = 144;
-        
+        while(true)
+        {
+            long t = (long) (triangle)*(triangle + 1)/2;
+            long p = (long) (pentagonal) * (3*pentagonal + 1)/2; 
+            long h = (long) (hexagonal) * (2*hexagonal-1);
+        }
     }
     
 }

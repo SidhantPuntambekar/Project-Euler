639b1668962fb5a7a3ce6c1b9c1eb45684ef3815
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-06-19 09:18:48 -0600

    Added next term of each sequence to main method

---
+++ Problem45.java
@@ -0,0 +1,11 @@
+public class Problem45
+{
+    public static public static void main(String[] args) 
+    {
+        int triangle = 286;
+        int pentagonal = 166;
+        int hexagonal = 144;
+        
+    }
+    
+}

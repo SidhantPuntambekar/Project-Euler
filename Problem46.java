404a88c84d6e3911a96efd914302f13987743dcd
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-06-20 11:49:09 -0600

    Added isGoldbach method

--- Problem46.java
+++ Problem46.java
@@ -1,5 +1,21 @@
 public class Problem46
 {
+    
+	public static boolean isGoldbach(int n) 
+    {
+        for(int i = 1; 2 * i * i < n; i++) 
+        {
+            for(int j = 3; j <= n - 2; j+=2) 
+            {
+                if(isPrime(j) && j + 2 * i * i == n) 
+                {
+					return true;
+				}
+			}
+		}
+		return false;
+    }
+    
     public static boolean isPrime(int n) 
     {
         if(n == 2) 

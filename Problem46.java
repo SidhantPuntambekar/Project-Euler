e70b3bb41bcc8587b829983e7e00573dd5b66cc7
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-06-20 11:47:21 -0600

    Added isPrime method

--- Problem46.java
+++ Problem46.java
@@ -1,7 +1,22 @@
 public class Problem46
 {
-    public static void main(String[] args) 
+    public static boolean isPrime(int n) 
     {
-        
-    }
+        if(n == 2) 
+        {
+			return true;
+		}
+        if(n == 1 || n % 2 == 0) 
+        {
+			return false;
+		}
+        for(int i = 3; i * i <= n; i += 2) 
+        {
+            if(n % i == 0) 
+            {
+				return false;
+			}
+		}
+		return true;
+	}
 }

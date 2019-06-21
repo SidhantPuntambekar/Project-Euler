e777821f421a8114f58c9e62ceda1d3b46b8db64
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-06-21 16:16:17 -0600

    Added countDistinctPrimeFactors method

--- Problem47.java
+++ Problem47.java
@@ -1,7 +1,26 @@
+import java.util.math;
 public class Problem47
 {
     public static void main(String[] args) 
     {
         
     }
+
+    public static int countDistinctPrimeFactors(int j) 
+    {
+		int count = 0;
+        for (int i = 2, end = Math.sqrt(j); i <= end; i++) 
+        {
+            if (n % i == 0) 
+            {
+				do j /= i;
+				while (j % i == 0);
+				count++;
+				end = Math.sqrt(n);
+			}
+		}
+		if (j > 1)
+			count++;
+		return count;
+	}
 }

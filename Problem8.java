41b74a32ef8582b2dd5e9a433e9e669d1748f9e6
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-06-26 09:36:32 -0600

    Finished Problem 8

--- Problem8.java
+++ Problem8.java
@@ -2,34 +2,22 @@ import java.io.File;
 import java.util.Scanner; 
 public class Problem8
 {
-    public static long ThirteenMultiples(int[] a)
-    {
-        long product = 1;
-        for (int i = 0; i < a.length; i++) 
-        {
-            long number = a[i] - '0';
-            product *= number;
-        }
-        return product;
-    }
-
     public static void main(String[] args) throws Exception 
     {
-        int[] digits = new int[1000];
-
-        // pass the path to the file as a parameter 
-        File file = new File("Problem8Input.txt"); 
-        Scanner sc = new Scanner(file);
-        int digitsInitialized = 0; 
-        while (sc.hasNextLine()) 
-        {
-            String fileLine = sc.nextLine();
-            for (int i = 0; i < fileLine.length(); i++) 
-            {
-                digits[digitsInitialized] = fileLine.charAt(i) - '0';
-                digitsInitialized++;
-            }
-        }
-        System.out.println(ThirteenMultiples(digits));
+        String num = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+		long largest = 0;
+		for(int i = 13; i < num.length(); i++){
+			String s = num.substring(i - 13, i);
+			long n = Long.valueOf(s).longValue();
+			long sum = 1;
+			while(n > 0) {
+				sum *= n % 10;
+				n /= 10;
+			}
+			if(sum > largest) {
+				largest = sum;
+			}
+		}
+		System.out.println(largest);
     } 
 }

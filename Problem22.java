e1d28001c95f8dec6a555ab1f9f404fce048f8ae
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-06-27 10:11:45 -0600

    Finished Problem 22

--- Problem22.java
+++ Problem22.java
@@ -1,41 +1,57 @@
-import java.util.scanner;
+import java.io.*;
+import java.util.Arrays;
 
 public class Problem22
 {
-    public void readFileToArray()
-    {
-        Scanner sc = new Scanner(new File(p022_names.txt));
-        List<String> lines = new ArrayList<String>();
-        while (sc.hasNextLine()) 
-        {
-            lines.add(sc.nextLine());
-        }
-        String[] arr = lines.toArray(new String[0]);
-        for (int i = 0; i < arr.length; i++)
-        {
-            arr[i].replaceAll(Pattern.quote("\""), "");
-        }
-    }
-
-    public static int values()
-    {
-        char[] Letters = {'1', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-        char[] tempName = s.toCharArray();
-
-        int sum = 0;
-        for (char c : tempName) 
-        {
-            for (int j = 0; j < ABC.length; j++) 
-            {
-                if (c == ABC[j])
-                    sum += j;
-            }
-        }
-        return sum;
-    }
-
     public static void main(String[] args)
     {
+        String[] names = new String[]{};
+		String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		int sum = 0;
+		BufferedReader reader = null;
+		
+        try 
+        {
+		    reader = new BufferedReader(new FileReader(new File("p022_names.txt")));
+		    String text = null;
 
+            while ((text = reader.readLine()) != null) 
+            {
+		    	text = text.substring(1, text.length() - 1);
+		        names = text.split("\",\"");
+		        Arrays.sort(names);
+                for(int i = 0; i < names.length; i++) 
+                {
+		        	int numericValue = 0;
+                    for(int j = 0; j < names[i].length(); j++) 
+                    {
+		        		numericValue += letters.indexOf(names[i].charAt(j)) + 1;
+		        	}
+		        	sum += numericValue * (i + 1);
+		        }
+		        System.out.println(sum);
+		    }
+        } 
+        catch (FileNotFoundException e) 
+        {
+		    e.printStackTrace();
+        } 
+        catch (IOException e) 
+        {
+		    e.printStackTrace();
+        } 
+        finally 
+        {
+            try 
+            {
+                if (reader != null) 
+                {
+		            reader.close();
+		        }
+            } 
+            catch (IOException e) 
+            {
+		    }
+		}
     }
 }

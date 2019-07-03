4201ca579671b1f5501afb46c6ad4e22f5ec5ab1
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-07-03 08:05:40 -0600

    Finished Problem 38

--- Problem38.java
+++ Problem38.java
@@ -3,32 +3,26 @@ public class Problem38
 {
     public static void main(String[] args) 
     {
-        int max = -1; 
-        String concatenated = "";
-        for (int i = 2; i <= 9; i++)
-        {
-            for (int j = 1; j < Math.pow(10, 9/i); j++)
-            {
-                concatenated = "";
-                for (int k = 1; k <= i; k++)
-                {
-                    concatenated += i * j;
-                }
-                if (isPandigital(concatenated))
-                {
-                    max = Math.max(Integer.parseInt(concatenated), max);
-                }
-            }
-        }
-        System.out.println(concatenated);
+        int largest = 0;
+		for(int i = 1; i < 10000; i++) {
+			String concatenated = "";
+			for(int j = 1; concatenated.length() < 9; j++) {
+				concatenated += i * j;
+			}
+			if(isPandigital(concatenated) && Integer.parseInt(concatenated) > largest) {
+				largest = Integer.parseInt(concatenated);
+			}
+		}
+		System.out.println(largest);
     }
 
     public static boolean isPandigital(String x) 
     {
-		if (x.length() != 9)
+		if(x.length() < 9) {
 			return false;
-		char[] temp = x.toCharArray();
-		Arrays.sort(temp);
-		return new String(temp).equals("123456789");
+		}
+		char[] c = x.toCharArray();
+		Arrays.sort(c);
+		return new String(c).equals("123456789");
 	}
 }

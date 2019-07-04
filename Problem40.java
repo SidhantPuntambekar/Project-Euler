c5222b3cbb39501c508554b4765e478c8b095fe9
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-07-04 11:53:43 -0600

    Continued expanding on four loop

--- Problem40.java
+++ Problem40.java
@@ -3,8 +3,18 @@ public class Problem40
     public static void main(String[] args) 
     {
         StringBuilder nums = new StringBuilder();
-		for(int i = 1; nums.length() <= 1000000; i++) {
+        for(int i = 1; nums.length() <= 1000000; i++) 
+        {
 			nums.append(i);
-		} 
+        }
+        int product = 1;
+        for(int j = 0; j < 6; j++) 
+        {
+			int k = Character.getNumericValue(nums.charAt((int) Math.pow(10, i) - 1));
+            if(k != 0) 
+            {
+				product *= d;
+            }
+        } 
     }
 }

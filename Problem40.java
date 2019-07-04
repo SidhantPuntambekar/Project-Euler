103467aef71680f5d83f11ed1e07fa47be913d18
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-07-04 11:01:26 -0600

    Started Problem 40

---
+++ Problem40.java
@@ -0,0 +1,10 @@
+public class Problem40
+{
+    public static void main(String[] args) 
+    {
+        StringBuilder nums = new StringBuilder();
+		for(int i = 1; nums.length() <= 1000000; i++) {
+			nums.append(i);
+		} 
+    }
+}

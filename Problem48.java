4dca4377d9695694100996647d6e6ec8f3fc3623
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-07-06 08:11:57 -0600

    Finished Problem 48

--- Problem48.java
+++ Problem48.java
@@ -1,6 +1,13 @@
+import java.math.BigInteger;
 public class Problem48
 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) 
+    {
+        BigInteger digits = BigInteger.ZERO;
+        for(int i = 1; i < 1000; i++) 
+        {
+			digits = digits.add(BigInteger.valueOf(i).modPow(BigInteger.valueOf(i), BigInteger.TEN.pow(10)));
+		}
+		System.out.println(digits.mod(BigInteger.TEN.pow(10)));
     }
 }

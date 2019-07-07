652e26415ddb463f1baf61cf0c5f1b4cba52811a
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-07-07 10:21:03 -0600

    Added isPermutation method

--- Problem49.java
+++ Problem49.java
@@ -4,4 +4,17 @@ public class Problem49
     {
         
     }
+
+    public static boolean isPermutation(int n, int m) 
+    {
+		String sn = String.valueOf(n);
+		String sm = String.valueOf(m);
+		char[] cn = sn.toCharArray();
+		char[] cm = sm.toCharArray();
+		Arrays.sort(cn);
+		Arrays.sort(cm);
+		sn = new String(cn);
+		sm = new String(cm);
+		return sn.equals(sm);
+	}
 }

9103212146b89128a866c573e87bda2d50a40cbd
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-07-11 08:56:26 -0600

    Finished main method

--- Problem49.java
+++ Problem49.java
@@ -1,8 +1,29 @@
+import java.util.Arrays;
+
 public class Problem49
 {
     public static void main(String[] args) 
     {
-        
+		int[] primes = listPrimes(10000, 1488);
+		String concatenated = "";
+		for (int i = 0; i <primes.length; i++)
+		{
+			for (int j = 0; j < primes.length; j++)
+			{
+				int k = primes[j] + (primes[j] - primes[i]);
+				if (k < 10000 && Arrays.binarySearch(primes, k) > -1)
+				{
+					if (isPermutation(primes[i], primes[j]) && isPermutation(primes[i], k))
+					{
+						concatenated = String.valueOf(primes[i]) + String.valueOf(primes[j]) + String.valueOf(k);
+						break;
+					}
+				}
+			}
+			if(concatenation.length() > 0)
+				break;
+		}
+		System.out.println(concatenated);
     }
 
 	public static int[] listPrimes(int n, int lowerBound)
@@ -25,7 +46,7 @@ public class Problem49
 			}
 		}
 		int count = 0; 
-		for (int i = lowerBound; j = 0; i < n; i++)
+		for (int i = lowerBound, j = 0; i < n; i++)
 		{
 			if(isPrime[i])
 			{

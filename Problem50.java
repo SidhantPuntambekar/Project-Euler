5c8556d2ca1afe6b7e8cdc8a847cb1e9a6bb9a67
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-07-12 10:37:22 -0600

    Added isPrime and nextPrime method

--- Problem50.java
+++ Problem50.java
@@ -4,4 +4,34 @@ public class Problem50
     {
         
     }
+
+    public static boolean isPrime(int n)
+    {
+        if(n == 2) 
+        {
+			return true;
+		}
+        if(n == 1 || n % 2 == 0) 
+        {
+			return false;
+		}
+        for(int i = 3; i * i <= n; i += 2) 
+        {
+            if(n % i == 0) 
+            {
+				return false;
+			}
+		}
+		return true;
+    }
+
+    public static int nextPrime(int n) 
+    {
+		int i = n + 2;
+        while(!isPrime(i)) 
+        {
+			i+=2;
+		}
+		return i;
+	}
 }

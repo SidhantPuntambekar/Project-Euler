8cb08b4f15bf13cbf8776a69ebfc345e6f9f0cde
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-07-17 09:33:52 -0600

    Created char array to store digits of n

--- Problem52.java
+++ Problem52.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Problem52
 {
     public static void main(String[] args) 
@@ -7,6 +9,7 @@ public class Problem52
 
     public static boolean sameDigits(int n)
     {
-
+        char[] ch = Integer.toString(n).toCharArray();
+        Arrays.sort(ch);
     }
 }

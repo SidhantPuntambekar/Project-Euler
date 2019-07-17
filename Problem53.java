fc1224c5d11205a0f8bac7a53ff230c1150be663
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-07-17 11:37:05 -0600

    Added factorial method for BigInteger

--- Problem53.java
+++ Problem53.java
@@ -4,4 +4,13 @@ public class Problem53
     {
         
     }
+
+    public static BigInteger factorial(int n, int r)
+    {
+        BigInteger factorial = BigInteger.ONE;
+		for(int i = r; i <= n; i++) {
+			factorial = factorial.multiply(BigInteger.valueOf(i));
+		}
+		return factorial;
+    }
 }

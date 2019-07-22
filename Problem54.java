bc0acaebe844c5a4452b284e29dfba2bdb81990a
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-07-22 15:46:04 -0600

    Added getHandValue method

--- Problem54.java
+++ Problem54.java
@@ -1,7 +1,19 @@
+import java.util.Arrays;
+
 public class Problem54
 {
     public static void main(String[] args) 
     {
         
     }
+
+    public static int getHandValue(int[][] h) 
+    {
+		int sum = 1;
+        for(int i = 0; i < h.length; i++) 
+        {
+			sum *= h[i][0];
+		}
+		return sum;
+	}
 }
